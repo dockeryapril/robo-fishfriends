c3c3972d92c1df798f583774857a2bae73c48146
aprildockery <dockeryapril@gmail.com>
2022-01-11 17:35:42 -0500

    update 1.11.22

--- src/robots.js
+++ src/robots.js
@@ -3,61 +3,51 @@ export const robots = [
       id: 1,
       name: 'Bass Graham',
       username: 'Bret',
-      email: 'Sincere@april.biz'
     },
     {
       id: 2,
       name: 'Smallmouth Howell',
       username: 'Antonette',
-      email: 'Shanna@melissa.tv'
     },
     {
       id: 3,
       name: 'Bluegill Bauch',
       username: 'Samantha',
-      email: 'Nathan@yesenia.net'
     },
     {
       id: 4,
       name: 'Shad Lebsack',
       username: 'Karianne',
-      email: 'Julianne.OConner@kory.org'
     },
     {
       id: 5,
       name: 'Drum Dietrich',
       username: 'Kamren',
-      email: 'Lucio_Hettinger@annie.ca'
     },
     {
       id: 6,
       name: 'Perch Schulist',
       username: 'Leopoldo_Corkery',
-      email: 'Karley_Dach@jasper.info'
     },
     {
       id: 7,
       name: 'Walleye Weissnat',
       username: 'Elwyn.Skiles',
-      email: 'Telly.Hoeger@billy.biz'
     },
     {
       id: 8,
-      name: 'Pumpkinseed Runolfsdottir V',
+      name: 'Pumpkinseed Waterdottir',
       username: 'Maxime_Nienow',
-      email: 'Sherwood@rosamond.me'
     },
     {
       id: 9,
       name: 'Golden Shiner Reichert',
       username: 'Delphine',
-      email: 'Chaim_McDermott@dana.io'
     },
     {
       id: 10,
       name: 'Salmon DuBuque',
       username: 'Moriah.Stanton',
-      email: 'Rey.Padberg@karina.biz'
     }
   ];
   
