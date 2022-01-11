233d232b14191489637600cb906c03a015aa365e
aprildockery <dockeryapril@gmail.com>
2022-01-10 23:20:20 -0500

    Update 1.10.22

--- src/robots.js
+++ src/robots.js
@@ -1,61 +1,61 @@
 export const robots = [
     {
       id: 1,
-      name: 'Leanne Graham',
+      name: 'Bass Graham',
       username: 'Bret',
       email: 'Sincere@april.biz'
     },
     {
       id: 2,
-      name: 'Ervin Howell',
+      name: 'Smallmouth Howell',
       username: 'Antonette',
       email: 'Shanna@melissa.tv'
     },
     {
       id: 3,
-      name: 'Clementine Bauch',
+      name: 'Bluegill Bauch',
       username: 'Samantha',
       email: 'Nathan@yesenia.net'
     },
     {
       id: 4,
-      name: 'Patricia Lebsack',
+      name: 'Shad Lebsack',
       username: 'Karianne',
       email: 'Julianne.OConner@kory.org'
     },
     {
       id: 5,
-      name: 'Chelsey Dietrich',
+      name: 'Drum Dietrich',
       username: 'Kamren',
       email: 'Lucio_Hettinger@annie.ca'
     },
     {
       id: 6,
-      name: 'Mrs. Dennis Schulist',
+      name: 'Perch Schulist',
       username: 'Leopoldo_Corkery',
       email: 'Karley_Dach@jasper.info'
     },
     {
       id: 7,
-      name: 'Kurtis Weissnat',
+      name: 'Walleye Weissnat',
       username: 'Elwyn.Skiles',
       email: 'Telly.Hoeger@billy.biz'
     },
     {
       id: 8,
-      name: 'Nicholas Runolfsdottir V',
+      name: 'Pumpkinseed Runolfsdottir V',
       username: 'Maxime_Nienow',
       email: 'Sherwood@rosamond.me'
     },
     {
       id: 9,
-      name: 'Glenna Reichert',
+      name: 'Golden Shiner Reichert',
       username: 'Delphine',
       email: 'Chaim_McDermott@dana.io'
     },
     {
       id: 10,
-      name: 'Clementina DuBuque',
+      name: 'Salmon DuBuque',
       username: 'Moriah.Stanton',
       email: 'Rey.Padberg@karina.biz'
     }
